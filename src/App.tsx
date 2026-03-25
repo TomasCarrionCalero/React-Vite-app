8e8c307c6ac5c78dd8f576a1464bf03ccff7325a
alumne <alumne@Dockers.dockers.local>
2026-03-25 18:40:31 +0100

    cambio2nombre

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const list = ["unoi", "dos", "tres"];
   return (
     <Card>
-      <CardBody title="Alo" text="Estees" />
+      <CardBody title="Aloo" text="Estees" />
       <List data={list} />
     </Card>
   );
