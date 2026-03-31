2aa18b9f3cdf3fc672f896b622532a4f2a37f1c9
alumne <alumne@Dockers.dockers.local>
2026-03-31 18:26:59 +0200

    listnuevo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 function App() {
-  const list = ["unoi", "dos", "tres"];
+  const list: string[] = [];
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+
   return (
     <Card>
-      <CardBody title="Aloo" text="Estees" />
-      <List data={list} />
+      <CardBody title="Hola" text="Este es" />
+      {list.length !== 0 ? <List data={list} onSelect={handleSelect} /> : "no"}
     </Card>
   );
 }
